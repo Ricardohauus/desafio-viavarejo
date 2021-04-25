d49732d21c3f529423f5fbcf9f763a81770c205f
Ricardo <ricardo.brasil@meta.com.br>
2021-04-25 13:43:40 -0300

    Criado o Mapper para a Apolice

---
+++ src/main/java/br/com/viavarejo/mapper/ApoliceMapper.java
@@ -0,0 +1,26 @@
+package br.com.viavarejo.mapper;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import br.com.viavarejo.dto.ApoliceDTO;
+import br.com.viavarejo.model.Apolice;
+
+public class ApoliceMapper {
+
+	public static ApoliceDTO convertEntityToApoliceDTO(Apolice a) {
+		ApoliceDTO apoliceDTO = new ApoliceDTO();
+		apoliceDTO.setCliente(a.getCliente());
+		apoliceDTO.setDataFim(a.getDataFim());
+		apoliceDTO.setDataInicio(a.getDataInicio());
+		apoliceDTO.setNumero(a.getNumero());
+		apoliceDTO.setPlacaVeiculo(a.getPlacaVeiculo());
+		apoliceDTO.setValor(a.getValor());
+		apoliceDTO.setSituacao(a.getDataFim().isBefore(LocalDate.now()) ? "Venceu" : "Em Vigência");
+		long dias = ChronoUnit.DAYS.between(LocalDate.now(), a.getDataFim());
+		apoliceDTO.setSituacaoEmDias(apoliceDTO.getSituacao().equals("Venceu") ? "Venceu há " + dias + " Dia(s)"
+				: "A sua Apolice vai vencer em " + dias + " Dia(s)");
+		return apoliceDTO;
+	}
+
+}
