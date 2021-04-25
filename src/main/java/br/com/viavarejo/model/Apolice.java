08b42a81150da27d5b0a3aaf00fe1f3721ab5410
Ricardo <ricardo.brasil@meta.com.br>
2021-04-25 00:47:16 -0300

    criando os modelos

---
+++ src/main/java/br/com/viavarejo/model/Apolice.java
@@ -0,0 +1,161 @@
+package br.com.viavarejo.model;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Document(collection = "apolice")
+public class Apolice {
+
+	@Id
+	@JsonIgnore
+	private String numero;
+
+	@NotNull(message = "Data Inicio {field.not.null}")
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataInicio;
+
+	@NotNull(message = "Data Fim {field.not.null}")
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataFim;
+
+	@NotEmpty(message = "Nome {field.not.blank}")
+	private String nome;
+
+	@NotEmpty(message = "Placa do Veiculo {field.not.blank}")
+	private String placaVeiculo;
+
+	@NotNull(message = "Valor {field.not.null}")
+	private Double valor;
+
+	@NotNull(message = "Cliente {field.not.null}")
+	private Cliente cliente;
+
+	@JsonProperty
+	public String getNumero() {
+		return numero;
+	}
+
+	@JsonIgnore
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlacaVeiculo() {
+		return placaVeiculo;
+	}
+
+	public void setPlacaVeiculo(String placaVeiculo) {
+		this.placaVeiculo = placaVeiculo;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+		result = prime * result + ((dataFim == null) ? 0 : dataFim.hashCode());
+		result = prime * result + ((dataInicio == null) ? 0 : dataInicio.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		result = prime * result + ((placaVeiculo == null) ? 0 : placaVeiculo.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Apolice other = (Apolice) obj;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
+		if (dataFim == null) {
+			if (other.dataFim != null)
+				return false;
+		} else if (!dataFim.equals(other.dataFim))
+			return false;
+		if (dataInicio == null) {
+			if (other.dataInicio != null)
+				return false;
+		} else if (!dataInicio.equals(other.dataInicio))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		if (placaVeiculo == null) {
+			if (other.placaVeiculo != null)
+				return false;
+		} else if (!placaVeiculo.equals(other.placaVeiculo))
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/br/com/viavarejo/model/Cliente.java
@@ -0,0 +1,123 @@
+package br.com.viavarejo.model;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Document(collection = "cliente")
+public class Cliente {
+
+	@JsonIgnore
+	@Id
+	private String id;
+
+	@NotEmpty(message = "Nome {field.not.blank}")
+	private String nome;
+
+	@NotEmpty(message = "CPF {field.not.blank}")
+	@CPF(message = "{field.cpf.invalidate}")
+	private String cpf;
+
+	@NotEmpty(message = "Cidade {field.not.blank}")
+	private String cidade;
+
+	@NotEmpty(message = "UF {field.not.blank}")
+	private String uf;
+
+	@JsonIgnore
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	@JsonProperty
+	public String getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (uf == null) {
+			if (other.uf != null)
+				return false;
+		} else if (!uf.equals(other.uf))
+			return false;
+		return true;
+	}
+
+}
